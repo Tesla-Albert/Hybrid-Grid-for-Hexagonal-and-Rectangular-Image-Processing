c8152cefcc5cd797becbd87cc72573ad1206cc9e
Tesla-Albert <1913439190@qq.com>
2025-08-11 15:51:26 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hygrid',
+    version='1.1.0',
+    description='A unified framework for rectangular and hexagonal grids',
+    author='Tesla Albert',
+    author_email='1913439190@qq.com',
+    packages=find_packages(),
+)
+
